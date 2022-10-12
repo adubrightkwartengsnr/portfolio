8e4a1fc491e3efc309caa3900c204cce54909743
Adu Bright Kwarteng snr <adubrightkwarrteng11@gmail.com>
2022-10-12 18:32:08 +0000

    finished working on the templates and created the blog model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 250)
+    image = models.ImageField(null = False,default="blog image")
+    description = models.TextField(null= False,default="Empty Blog")
+    date_created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
